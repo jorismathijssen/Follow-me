24a362e732ab6a767e7b3b648b86dbbf70e2df19
Joris Mathijssen <Jorismathijssen@gmail.com>
2015-03-06 16:57:54 +0100

    We had cleaning to do

--- html/index.php
+++ html/index.php
@@ -1,198 +1,186 @@
 <!DOCTYPE html>
 <html>
-
     <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-        <title>Follow Me - Joris Mathijssen</title>
-
-        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false" type="text/javascript"></script>
+        <meta charset="utf-8">
+        <title>Street View service</title>
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
         <script src="http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js" type="text/javascript"></script>
-        <script src="js/dat.gui.min.js"></script>
-        <script src="js/three.min.js"></script>
-        <script src="js/GSVPano.js"></script>
-        <script src="../js/Hyperlapse.js"></script>
         <script>
-            //creating start up vars.
-            var start_point = new google.maps.LatLng(48.85877000000001, 2.293130000000019);
-            var end_point = new google.maps.LatLng(48.859260000000006, 2.2938300000000003);
-            var lookat_point = new google.maps.LatLng(48.858228924927595, 2.294524215344154);
-            var map, directions_renderer, directions_service, streetview_service, geocoder;
-            var start_pin, end_pin, pivot_pin, camera_pin;
-            var _elevation = 0;
-            var _route_markers = [];
+            //creating the array's before use globaly, is this right?
+            var addresses = [], counter = 0;
+            var titel = [];
+            var desc = [];
+            var heading = [];
+            var pitch = [];
 
-            function init() {
+            function initialize() {
+                //Ajax call to get the cordinates
+                $.ajax({
+                    url: 'getcords.php',
+                    data: "dataString",
+                    dataType: 'html',
+                    success: function(data)
+                    {
+                        var obj = JSON && JSON.parse(data) || $.parseJSON(data);
+                        for (var key in obj) {
+                            //Put data in the array's
+                            addresses.push(new google.maps.LatLng(obj[key]['latitude'], obj[key]['longitude']));
+                            titel.push(obj[key]['titel']);
+                            desc.push(obj[key]['beschrijving']);
+                            heading.push(obj[key]['heading']);
+                            pitch.push(obj[key]['pitch']);
+                        }
+                        console.log('WTF');
+                        var location = addresses[0]; //seting the first location this way.
+                        var mapOptions = {
+                            center: location,
+                            zoom: 14
+                        }; //set the map options for top left map
+                        var map = new google.maps.Map(
+                            document.getElementById('map-canvas'), mapOptions);
+                        var panoramaOptions = {
+                            position: location,
+                            zoom: 0.2,
+                            pov: {
+                                heading: parseFloat(heading[0]),
+                                pitch: parseFloat(pitch[0])
+                            },
+                            disableDefaultUI: true
 
-                if (window.location.hash) {
-                    parts = window.location.hash.substr(1).split(',');
-                    start_point = new google.maps.LatLng(parts[0], parts[1]);
-                    lookat_point = new google.maps.LatLng(parts[2], parts[3]);
-                    end_point = new google.maps.LatLng(parts[4], parts[5]);
-                    _elevation = parts[6] || 0;
-                }
+                        };//set the map options for panorama map
+                        var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+                        map.setStreetView(panorama);
 
-                /* Map */
+                        //add buttons to field.
 
-                function snapToRoad(point, callback) {
-                    var request = {
-                        origin: point,
-                        destination: point,
-                        travelMode: google.maps.TravelMode["DRIVING"]
-                    };
-                    directions_service.route(request, function (response, status) {
-                        if (status == "OK") callback(response.routes[0].overview_path[0]);
-                        else callback(null);
-                    });
-                }
+                        var leftControlDiv = document.createElement('div');
+                        var leftControler = new leftControl(leftControlDiv, panorama, map);
 
-                function changeHash() {
-                    window.location.hash = start_pin.getPosition().lat() + ',' + start_pin.getPosition().lng() + ',' + pivot_pin.getPosition().lat() + ',' + pivot_pin.getPosition().lng() + ',' + end_pin.getPosition().lat() + ',' + end_pin.getPosition().lng() + ',' + _elevation;
-                }
+                        var rightControlDiv = document.createElement('div');
+                        var rightControler = new rightControl(rightControlDiv, panorama, map);
 
-                var mapOpt = {
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    center: start_point,
-                    zoom: 15
-                };
+                        leftControlDiv.index = 1;
+                        panorama.controls[google.maps.ControlPosition.BOTTOM].push(leftControlDiv);
 
-                var start = 'img/start.png';
-                var looks = 'img/eye.png';
-                var eind = 'img/eind.png';
+                        rightControlDiv.index = 1;
+                        panorama.controls[google.maps.ControlPosition.BOTTOM].push(rightControlDiv);
 
-                map = new google.maps.Map(document.getElementById("map"), mapOpt);
-                geocoder = new google.maps.Geocoder();
+                        //setting the titel and discription divs
+                        var titelElement = document.getElementById("header");
+                        titelElement.innerHTML = titel[0];
+                        var descElement = document.getElementById("desc");
+                        descElement.innerHTML = desc[0];
 
-                var overlay = new google.maps.StreetViewCoverageLayer();
-                overlay.setMap(map);
-
-                directions_service = new google.maps.DirectionsService();
-                directions_renderer = new google.maps.DirectionsRenderer({
-                    draggable: false,
-                    markerOptions: {
-                        visible: false
+                        //Creating a listener for the image click.
+                        $("#imagebox").click(function(){
+                            console.log('Hallo');
+                        });
                     }
-                });
-                directions_renderer.setMap(map);
-                directions_renderer.setOptions({
-                    preserveViewport: true
-                });
-
-                camera_pin = new google.maps.Marker({
-                    position: start_point,
-                    map: map,
-                    visible: false
-                });
-
-                start_pin = new google.maps.Marker({
-                    position: start_point,
-                    draggable: true,
-                    map: map,
-                    icon: start
-                });
-
-                google.maps.event.addListener(start_pin, 'dragend', function (event) {
-                    snapToRoad(start_pin.getPosition(), function (result) {
-                        start_pin.setPosition(result);
-                        start_point = result;
-                        changeHash();
-                    });
-                });
-
-                end_pin = new google.maps.Marker({
-                    position: end_point,
-                    draggable: true,
-                    map: map,
-                    icon: eind
-                });
-
-                google.maps.event.addListener(end_pin, 'dragend', function (event) {
-                    snapToRoad(end_pin.getPosition(), function (result) {
-                        end_pin.setPosition(result);
-                        end_point = result;
-                        changeHash();
-                    });
-                });
-
-                pivot_pin = new google.maps.Marker({
-                    position: lookat_point,
-                    draggable: true,
-                    map: map,
-                    icon: looks
-                });
-
-                google.maps.event.addListener(pivot_pin, 'dragend', function (event) {
-                    hyperlapse.setLookat(pivot_pin.getPosition());
-                    changeHash();
-                });
-
-                function findAddress(address) {
-                    geocoder.geocode({
-                        'address': address
-                    }, function (results, status) {
-                        if (status == google.maps.GeocoderStatus.OK) {
-                            map.setCenter(results[0].geometry.location);
-                            o.drop_pins();
-                        } else {
-                            console.log("Geocode was not successful for the following reason: " + status);
-                        }
-                    });
-                }
-
-                var search = document.getElementById('searchButton');
-                search.addEventListener('click', function (event) {
-                    event.preventDefault();
-                    findAddress(document.getElementById("address").value);
-                }, false);
-
-                //Save cords function
-                save.addEventListener('click', function(event) {
-                    var lat = pivot_pin.getPosition().lat();
-                    var lng = pivot_pin.getPosition().lng();
-                    //use an AJAX function to save the lat/lng to the data base
-                    $.ajax({
-                        type: 'POST',
-                        data: {
-                            latitude: lat,
-                            longitude: lng
-                        },
-                        url: "savecords.php",
-                        success: function(response) {
-                            if (response == "success") {
-                                console.log(response);
-                            } else {
-                                console.log(response);
-                            }
-                        },
-                        error: function() {
-                            alert('An error occured');
-                        }
-                    });
                 });
             }
 
-            window.onload = init;
+
+            /*TODO: Check why left click needs 2 clicks and right click needs 1 before working*/
+
+            function leftControl(controlDiv, map, smallmap) {
+
+                // Set CSS for the control border
+                var controlUI = document.createElement('div');
+                controlUI.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+                controlUI.style.border = '2px solid rgba(0, 0, 0, 0.7)';
+                controlUI.style.borderRadius = '3px';
+                controlUI.style.boxShadow = '0 2px 6px rgba(0, 0, 0, 0.7)';
+                controlUI.style.cursor = 'pointer';
+                controlUI.style.marginBottom = '22px';
+                controlUI.style.marginRight = '22px';
+                controlUI.style.textAlign = 'center';
+                controlUI.title = 'To the the left';
+                controlDiv.appendChild(controlUI);
+
+                // Set CSS for the control interior
+                var controlText = document.createElement('div');
+                controlText.style.color = 'rgb(244, 235, 235)';
+                controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+                controlText.style.fontSize = '16px';
+                controlText.style.lineHeight = '38px';
+                controlText.style.paddingLeft = '5px';
+                controlText.style.paddingRight = '5px';
+                controlText.innerHTML = 'Vorige';
+                controlUI.appendChild(controlText);
+
+                // Setup the click event listeners
+                google.maps.event.addDomListener(controlUI, 'click', function() {
+                    if(counter == 0) counter = addresses.length; //check if counter is 0 otherwise set to 0. There is a fix for this...
+                    counter = (counter - 1);
+                    smallmap.panTo(addresses[counter]);
+                    map.setPosition(addresses[counter]);
+                    var pov = map.getPov();
+                    pov.heading = parseFloat(heading[counter]);
+                    pov.pitch = parseFloat(pitch[counter]);
+                    map.setPov(pov);
+                    var titelElement = document.getElementById("header");
+                    titelElement.innerHTML = titel[counter];
+                    var descElement = document.getElementById("desc");
+                    descElement.innerHTML = desc[counter];
+                });
+
+            }
+
+            function rightControl(controlDiv, map, smallmap) {
+
+                // Set CSS for the control border
+                var controlUI1 = document.createElement('div');
+                controlUI1.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+                controlUI1.style.border = '2px solid rgba(0, 0, 0, 0.7)';
+                controlUI1.style.borderRadius = '3px';
+                controlUI1.style.boxShadow = '0 2px 6px rgba(0, 0, 0, 0.7)';
+                controlUI1.style.cursor = 'pointer';
+                controlUI1.style.marginBottom = '22px';
+                controlUI1.style.textAlign = 'center';
+                controlUI1.title = 'To the the right';
+                controlDiv.appendChild(controlUI1);
+
+                // Set CSS for the control interior
+                var controlText1 = document.createElement('div');
+                controlText1.style.color = 'rgb(244, 235, 235)';
+                controlText1.style.fontFamily = 'Roboto,Arial,sans-serif';
+                controlText1.style.fontSize = '16px';
+                controlText1.style.lineHeight = '38px';
+                controlText1.style.paddingLeft = '5px';
+                controlText1.style.paddingRight = '5px';
+                controlText1.innerHTML = 'Volgende';
+                controlUI1.appendChild(controlText1);
+
+                // Setup the click event listeners
+                google.maps.event.addDomListener(controlUI1, 'click', function() {
+                    counter = (counter + 1) % addresses.length;
+                    smallmap.panTo(addresses[counter]);
+                    map.setPosition(addresses[counter]);
+                    var pov = map.getPov();
+                    pov.heading = parseFloat(heading[counter]);
+                    pov.pitch = parseFloat(pitch[counter]);
+                    map.setPov(pov);
+                    var titelElement = document.getElementById("header");
+                    titelElement.innerHTML = titel[counter];
+                    var descElement = document.getElementById("desc");
+                    descElement.innerHTML = desc[counter];
+                });
+
+            }
+
+            google.maps.event.addDomListener(window, 'load', initialize);
+
         </script>
     </head>
-
     <body>
-        <div id="pano" style="position: absolute; left: 0; top: 0; right: 0; bottom: 0; z-index:-1;"></div>
-        <div id="controls">
-            <div id="map" style="width: 800px; height: 600px; float: left; padding: 0;"></div>
-            <div id="controls" style="">
-                <form id="map_form">
-                    <input type="text" name="address" id="address" />
-                    <button type="submit" id="searchButton">Search</button>
-                    <button id="save">Sla punten op</button>
-                </form>
+        <div id="pano">
+            <div id="map-canvas">
             </div>
-            <div style="float:right;">
-                <input type="text" name="routename" id="routename" placeholder="route name"/>
-                <button id="routesave">Sla route op</button>
-            </div>
-            <textarea rows="30" cols="65" id="output"></textarea>
-            <button id="tester">test</button>
+            <div id="imagebox"><img src="img/eiffeltoren.jpg"></div>
+            <div id="hintbox-top"><h1 id="header"></h1><p id="desc"></p></div>
         </div>
-
     </body>
-
 </html>
+
